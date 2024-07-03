156df5573ef6fdbce9743d5c4534ebf0ab0f8d1a
Jesus Glez <jesusgleztr94@gmail.com>
2024-07-03 16:38:34 -0600

    refactor(Web): :recycle: Agregando PHPMailer

--- enviar_correo.php
+++ enviar_correo.php
@@ -1,5 +1,10 @@
 <?php
 
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'vendor/autoload.php';
 
 function loadEnv($path)
 {
@@ -25,40 +30,57 @@ function loadEnv($path)
 	}
 }
 
-// Usar la función para cargar las variables de entorno
-
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
 	if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 		loadEnv(__DIR__ . '/.env');
 
-		$mailFrom = getenv('MAIL_FROM');
+		try {
+			$mail = new PHPMailer();
 
-		$contact = $_POST['contact'];
-		$project = $_POST['project'];
-		$looking = $_POST['looking'];
-		$other = $_POST['other'] ?? null;
-		$share = $_POST['share'];
+			$mailFrom = getenv('MAIL_FROM');
 
-		$to = $email;
-		$subject = "Partnerships, Integrations & Collaborations";
-		$headers = "From: $mailFrom";
+			$contact = $_POST['contact'];
+			$project = $_POST['project'];
+			$looking = $_POST['looking'];
+			$other = $_POST['other'] ?? null;
+			$share = $_POST['share'];
 
-		$message = "A person left their information about Partnerships, Integrations & Collaborations: \n\n";
-		$message .= "Contact: $contact \n";
-		$message .= "Project: $project \n";
-		$message .= "Looking for: $looking \n";
-		if ($other) $message .= "Write your answer about what you are looking for: $other \n";
-		$message .= "Share more context: $share \n";
+			//Server settings
+			$mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
+			$mail->isSMTP();                                            //Send using SMTP
+			$mail->Host       = getenv('MAIL_HOST');                    //Set the SMTP server to send through
+			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+			$mail->Username   = getenv('MAIL_USERNAME');                //SMTP username
+			$mail->Password   = getenv('MAIL_PASSWORD');                //SMTP password
+			$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+			$mail->Port       = getenv('MAIL_PORT');                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-		if (mail($to, $subject, $message, $headers)) {
-			http_response_code(200);
-			echo json_encode(["message" => "Correo enviado exitosamente!"]);
-		} else {
+			//Recipients
+			$mail->setFrom(getenv('MAIL_FROM_ADDRESS'), getenv('MAIL_FROM_NAME'));
+			$mail->addAddress(getenv('MAIL_TO_ADDRESS'));
+
+			//Content
+			$mail->isHTML(true);                                        //Set email format to HTML
+			$mail->Subject = "Partnerships, Integrations & Collaborations";
+
+			$message = "A person left their information about Partnerships, Integrations & Collaborations: <br /><br />";
+			$message .= "Contact: $contact <br />";
+			$message .= "Project: $project <br />";
+			$message .= "Looking for: $looking <br />";
+			if ($other) $message .= "Write your answer about what you are looking for: $other <br />";
+			$message .= "Share more context: $share <br />";
+
+			$mail->Body = $message;
+
+			if ($mail->send()) {
+				http_response_code(200);
+				echo json_encode(["message" => "Correo enviado exitosamente!"]);
+			} else throw new Exception($mail->ErrorInfo);
+		} catch (Exception $e) {
 			http_response_code(500);
-			echo json_encode(["message" => "Error al enviar el correo."]);
+			echo json_encode(["message" => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"]);
 		}
 	} else {
 		http_response_code(400);
