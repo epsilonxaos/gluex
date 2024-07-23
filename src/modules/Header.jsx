bcfd9fd59bef25c4beb2d394c523d512d9e70043
Epsilonxaos <angelotr12@hotmail.com>
2024-07-22 20:53:21 -0600

    refactor(Web): :recycle: Agregando backdrop

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import logo from "../assets/img/logo.svg";
 import Links from "../components/Links";
 import { AnimatePresence, motion } from "framer-motion";
@@ -35,40 +35,49 @@ export const Header = () => {
 		setOpen(false);
 	});
 
-	return (
-		<header ref={ref} className="absolute px-10 w-full z-50">
-			<div className="max-w-app mx-auto">
-				<nav className="grid grid-cols-2 py-7 items-center">
-					<div className="col-span-1">
-						<img src={logo} className="w-[95px]" alt="Glue X" />
-					</div>
-					<div className="col-span-1 flex items-center justify-end">
-						<AnimatePresence>
-							{open && (
-								<nav className="mr-12">
-									<Menu className="hidden items-center justify-end gap-12 text-xs font-auxMono md:flex" />
-									<Menu className="absolute top-full text-right right-10 text-xs font-auxMono md:hidden" />
-								</nav>
-							)}
-						</AnimatePresence>
+	useEffect(() => {
+		if (open) document.querySelector("body").classList.add("overflow-hidden");
+		else document.querySelector("body").classList.remove("overflow-hidden");
+	}, [open]);
 
-						<button type="button" className="h-[30px] w-[30px] flex items-center justify-center" onClick={handleToggle}>
-							{!open ? (
-								<svg className="w-[30px] scale-[.65] block cursor-pointer" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 89.1 68.8" xmlSpace="preserve">
-									<g>
-										<path fill="white" d="M3.8,68.8h81.4c2.1,0,3.8-1.7,3.8-3.8v-3.3c0-2.1-1.7-3.8-3.8-3.8H3.8c-2.1,0-3.8,1.7-3.8,3.8L0,65 C0,67.1,1.7,68.8,3.8,68.8z" />
-										<path fill="white" d="M25,39.9h60.2c2.1,0,3.8-1.7,3.8-3.8v-3.3c0-2.1-1.7-3.8-3.8-3.8H25c-2.1,0-3.8,1.7-3.8,3.8V36 C21.2,38.2,22.9,39.9,25,39.9z" />
-										<path fill="white" d="M3.8,11h81.4c2.1,0,3.8-1.7,3.8-3.8V3.8c0-2.1-1.7-3.8-3.8-3.8L3.8,0C1.7,0,0,1.7,0,3.8l0,3.3 C0,9.3,1.7,11,3.8,11z" />
-									</g>
-								</svg>
-							) : (
-								<IoCloseOutline size={30} className="relative" />
-							)}
-						</button>
-					</div>
-				</nav>
-			</div>
-		</header>
+	return (
+		<>
+			<header ref={ref} className="absolute px-10 w-full z-50">
+				<div className="max-w-app mx-auto">
+					<nav className="grid grid-cols-2 py-7 items-center">
+						<div className="col-span-1">
+							<img src={logo} className="w-[95px]" alt="Glue X" />
+						</div>
+						<div className="col-span-1 flex items-center justify-end">
+							<AnimatePresence>
+								{open && (
+									<nav className="mr-12">
+										<Menu className="hidden items-center justify-end gap-12 text-xs font-auxMono md:flex" />
+										<Menu className="absolute top-full text-right right-10 text-xs font-auxMono md:hidden" />
+									</nav>
+								)}
+							</AnimatePresence>
+
+							<button type="button" className="h-[30px] w-[30px] flex items-center justify-center" onClick={handleToggle}>
+								{!open ? (
+									<svg className="w-[30px] scale-[.65] block cursor-pointer" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 89.1 68.8" xmlSpace="preserve">
+										<g>
+											<path fill="white" d="M3.8,68.8h81.4c2.1,0,3.8-1.7,3.8-3.8v-3.3c0-2.1-1.7-3.8-3.8-3.8H3.8c-2.1,0-3.8,1.7-3.8,3.8L0,65 C0,67.1,1.7,68.8,3.8,68.8z" />
+											<path fill="white" d="M25,39.9h60.2c2.1,0,3.8-1.7,3.8-3.8v-3.3c0-2.1-1.7-3.8-3.8-3.8H25c-2.1,0-3.8,1.7-3.8,3.8V36 C21.2,38.2,22.9,39.9,25,39.9z" />
+											<path fill="white" d="M3.8,11h81.4c2.1,0,3.8-1.7,3.8-3.8V3.8c0-2.1-1.7-3.8-3.8-3.8L3.8,0C1.7,0,0,1.7,0,3.8l0,3.3 C0,9.3,1.7,11,3.8,11z" />
+										</g>
+									</svg>
+								) : (
+									<IoCloseOutline size={30} className="relative" />
+								)}
+							</button>
+						</div>
+					</nav>
+				</div>
+			</header>
+
+			{open && <div className="bg-black lg:hidden bg-opacity-70 backdrop-blur-sm absolute top-0 left-0 w-full h-svh z-20"></div>}
+		</>
 	);
 };
 
